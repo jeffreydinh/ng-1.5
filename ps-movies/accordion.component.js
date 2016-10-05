4489bcfde196a7b445acae5b32e57ae40ffccf06
GLIDEWELLLAB\jeffrey.dinh <jeffrey.dinh@glidewelldental.com>
2016-10-05 14:04:15 -0700

    implementing the accordion (panel heading)

--- ps-movies/accordion.component.js
+++ ps-movies/accordion.component.js
@@ -13,8 +13,15 @@
     });
 
     module.component("accordionPanel", {
+        bindings: {
+            "heading": "@"
+        },
+        controllerAs: "vm",
         transclude: true,
         template: "<div class='panel panel-default'>" +
+            "<div class='panel-heading'>" +
+            "<h4 class='panel-title'>{{vm.heading}}</h4>" +
+        "</div>" +
         "<div class='panel-body' ng-transclude>" +
         "</div>" +
         "</div>"
--- ps-movies/movie-rating.component.js
+++ ps-movies/movie-rating.component.js
@@ -27,12 +27,16 @@
             // "<" tells angular that this is an input - this value will be given to me from the outside world
             // max "<" is also an input
             /**
-             * @ - read attributes
+             * @ - read attributes binding
              * & - function bindings
              * = - two way binding
              * < - input binding
              *
              * event - whenever the user click on a star, from this component, invoke this setRating number and pass along the new value
+             * <, = - angular goes to the element (accordionPanel), look at this attribute, and treat the value here as an expression
+             * meaning goes to current scope object, look for a property called Panel 1, so that it can read that property value and convert whatever that yields into a string that it can put onto the screen
+             *
+             * @ - have text appear without interpolation, without treating it as an expression
              */
             value: "<",
             max: "<",
